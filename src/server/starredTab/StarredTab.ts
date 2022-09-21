8c6aa68c40322ece35fc50aebb0d6ac0889c41fd
Vinita Tahiliani <vinita.tahiliani@microsoft.com>
2022-09-21 13:23:29 +0530

    Added src file changes

--- src/server/starredTab/StarredTab.ts
+++ src/server/starredTab/StarredTab.ts
@@ -4,6 +4,6 @@ import { PreventIframe } from "express-msteams-host";
  * Used as place holder for the decorators
  */
 @PreventIframe("/starredTab/index.html")
-@PreventIframe("/starredTab/starred.html")
+@PreventIframe("/starredTabTab/starred.html")
 export class StarredTab {
 }
